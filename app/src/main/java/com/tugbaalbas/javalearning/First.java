8d4ae23a89e049322a45dceae2d51946350325d1
tugba albas <tugbaalbas@tugbas-Air.home>
2023-03-07 01:56:39 +0300

    Initial commit

---
+++ app/src/main/java/com/tugbaalbas/javalearning/First.java
@@ -0,0 +1,10 @@
+package com.tugbaalbas.javalearning;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello java!");
+        System.out.println(5*2);
+        System.out.println("hello world!");
+
+    }
+}
